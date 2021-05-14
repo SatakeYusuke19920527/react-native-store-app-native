b7309da661cea06523715d51fd21bdf3749e1740
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-05-14 09:03:31 +0900

    commit

--- src/types/navigation.ts
+++
@@ -1,9 +0,0 @@
-import {Shop} from './Shop'
-
-export type RootStackParamList = {
-  Main: undefined,
-  Home: undefined,
-  Shop: { shop: Shop },
-  User: undefined,
-  CreateReview: {shop: Shop}
-}
