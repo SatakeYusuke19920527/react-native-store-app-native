e86d5c5019caddaf94632f1cafba931ef97e7b20
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-05-17 09:00:53 +0900

    [add] functions add

--- shop-review-functions/functions/src/index.ts
+++ shop-review-functions/functions/src/index.ts
@@ -1,9 +1,26 @@
 import * as functions from "firebase-functions";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+import { User } from './types/User'
+exports.onUpdateUser = functions
+  .region('asia-northeast2')
+    .firestore
+    .document('users/{userId}')
+    .onUpdate(async (change, context) => {
+      const { userId } = context.params
+      const newUser = change.after.data() as User
+      try {
+        const snapshot = await db.collectionGroup('reviews').where('user.id', '==', userId).get()
+        const batch = db.batch()
+        snapshot.docs.forEach((reviewDoc: any) => {
+          const user = { ...reviewDoc.data().user, name: newUser.name }
+          batch.update(reviewDoc.ref, {user})
+        });
+        await batch.commit()
+      } catch (error) {
+        console.log(error)
+      }
+    });
